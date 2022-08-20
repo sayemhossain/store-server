66cf4ecb8d5ccee061d48a24681f2a40379a7689
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-21 01:02:48 +0600

    new code added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// this is midlewiere
+// this is midlewier
 app.use(cors());
 app.use(express.json());
 
